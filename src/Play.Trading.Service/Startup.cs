c4a5d56c4b50fc5da5a2696ef0ba5d1b74208a81
docteurjay <aristejean@gmail.com>
2022-04-08 23:15:19 -0400

    Configuring the Trading Microservice

--- src/Play.Trading.Service/Startup.cs
+++ src/Play.Trading.Service/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Play.Common.Identity;
+using Play.Common.MassTransit;
+using Play.Common.MongoDB;
+using Play.Common.Settings;
+using Play.Trading.Service.StateMachines;
 
 namespace Play.Trading.Service
 {
@@ -26,6 +32,10 @@ namespace Play.Trading.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMongo()
+                    .AddJwtBearerAuthentication();
+
+            AddMassTransit(services);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -48,12 +58,34 @@ namespace Play.Trading.Service
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void AddMassTransit(IServiceCollection services)
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.UsingPlayEconomyRabbitMq();
+                configure.AddSagaStateMachine<PurchaseStateMachine, PurchaseState>()
+                        .MongoDbRepository(r =>
+                        {
+                            var servicesSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                            var mongoSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+
+                            r.Connection = mongoSettings.ConnectionString;
+                            r.DatabaseName = servicesSettings.ServiceName;
+                        });
+            });
+
+            services.AddMassTransitHostedService();
         }
     }
 }
