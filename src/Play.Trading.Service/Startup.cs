f9edf7327daa9484d3c583fc877dcb4f8de6dc8f
docteurjay <aristejean@gmail.com>
2022-05-15 00:00:41 -0400

    Added The Health Checks

--- src/Play.Trading.Service/Startup.cs
+++ src/Play.Trading.Service/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Play.Common.HealthChecks;
 using Play.Common.Identity;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
@@ -58,6 +59,9 @@ namespace Play.Trading.Service
             services.AddSingleton<IUserIdProvider, UserIdProvider>()
                     .AddSingleton<MessageHub>()
                     .AddSignalR();
+
+            services.AddHealthChecks()
+                    .AddMongoDb();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -90,6 +94,7 @@ namespace Play.Trading.Service
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<MessageHub>("/messageHub");
+                endpoints.MapPlayEconomyHealthChecks();
             });
         }
 
